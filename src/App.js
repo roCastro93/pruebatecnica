f71946f0419e40d55215a8b16791a500622fe931
Rodolfo Castro <chambeke_16@gmail.com>
2021-03-25 22:41:49 -0300

    agregando comentarios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Vista from './components/Vista';
 import Axios from 'axios';
 
-
 function App() {
+  //hook a utilizar
+  const [resultado, setResultado] = useState({});
+  const [precio, setPrecio] = useState({});
+  const [descripcion, setDescription] = useState({});
+  const [garantia, setGaratia] = useState({});
 
+
+//funcion de llamado a la API
   const consultarApi = async () => {
 
-    const url = `https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763`
-
-    const resultado = await Axios(url);
-
-    console.log(resultado);
+    const url = (`https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763`
+    );
+    const resultado = await Axios.get(url);
+    //Set de hooks
+    setResultado(resultado.data[0]);
+    setPrecio(resultado.data[0].prices);
+    console.log(resultado.data[0].prices);
   }
 
   useEffect(() => {
     consultarApi();
-  });
+  }, [descripcion])
 
 
 
   return (
     <div className="App">
-      <Vista />
+      <Vista
+        resultado={resultado}
+        precio={precio}
+      />
     </div>
   );
 }
--- src/components/Vista.js
+++ src/components/Vista.js
@@ -1,9 +1,33 @@
 
 
-const Vista =( ) =>{
-    return(
-        <div>
-            <h1>Hola desde vista</h1>
+const Vista = ({ resultado, precio }) => {
+
+
+    return (
+        //cards para mostrar la vista
+        <div className="container">
+            <div className="card mb-3" >
+                <div className="row g-0">
+                    <div className="col-md-8">
+                        <img src={resultado.fullImage} alt="..." className="img-fluid img-thumbnail" />
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card-body">
+                            <h4>APPLE</h4>
+                            <h5 className="card-title">{resultado.name}</h5>
+                            <p>{resultado.partNumber}</p>
+                            <p className="card-text">{resultado.shortDescription}</p>
+                            <p className="card-text">{precio.formattedCardPrice}<span>  -11%</span> </p>
+                            <p className="card-text"><strong>Internet: </strong>{precio.formattedOfferPrice}</p>
+                            <p className="card-text">Normal: <strike>{precio.formattedListPrice}</strike></p>
+                            <div className="row justify-content-between" >
+                                <p>Con tarjeta Ripley acumulas <strong>{precio.ripleyPuntos}</strong> RipleyPuntos GO</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
